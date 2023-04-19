45950cc3279d4446041eee720ee551abb368d91d
Gustavo Weber Denardin <gustavo.denardin@gmail.com>
2023-04-19 15:05:19 -0300

    add model7 data for month and year

--- src/models/model7.rs
+++ src/models/model7.rs
@@ -3,12 +3,14 @@ use super::*;
 pub fn model7() -> HModel {
     let mut ret = HModel {
         start_addr: 32114,
-        end_addr: 32115,
+        end_addr: 32119,
         model_number: 7,
-        qtd: 2,
+        qtd: 6,
         data: Vec::new(),
     };
 
     ret.data.push(HDataTypes::HuaweiU32(Point { name: "Daily energy yield", offset: 0, length: 2, write_access: false, value: 0 } ));
+    ret.data.push(HDataTypes::HuaweiU32(Point { name: "Month energy yield", offset: 2, length: 2, write_access: false, value: 0 } ));
+    ret.data.push(HDataTypes::HuaweiU32(Point { name: "Year energy yield", offset: 4, length: 2, write_access: false, value: 0 } ));
     ret
 }
